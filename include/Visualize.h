ea7d9961b95eb374204cd9bd7531134e474d4ea3
Eual11 <uchihaeual11@gmail.com>
2024-04-19 01:56:23 +0300

    inital commit: visualize

---
+++ include/Visualize.h
@@ -0,0 +1,8 @@
+#ifndef _VISUALIZE_H
+#define _VISUALIZE_H
+#include <matplot/matplot.h>
+#include <string>
+#include <vector>
+void Graph_Word_Freq(std::vector<std::string> &words,
+                     std::vector<long long> &freq);
+#endif
