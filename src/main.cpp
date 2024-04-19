378c0406725c1b0de7b14a7251f0d49345702b1d
Eual11 <uchihaeual11@gmail.com>
2024-04-20 00:23:41 +0300

    minor improvements and added command line args support (kinda)

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,39 @@
 #include "../include/Tokenizer.h"
+#include <matplot/freestanding/plot.h>
 #include <matplot/matplot.h>
 void Graph_Word_Freq(std::vector<std::string> &words,
                      std::vector<long long> &freq);
 void Graph_Zipfs(const std::vector<long long> &freq);
-int main() {
-  Tokenizer token("../tool/data/frequency_data.txt", 100000,
-                  Tokenizer::DATAFORMAT::MAP);
+int main(int argc, char **argv) {
+
+  int SAMPLE_SIZE = 1000;
+  char path_to_data[] = "../tool/data/frequency_data.txt";
+
+  std::string pathString(path_to_data);
+  Tokenizer::DATAFORMAT format = Tokenizer::DATAFORMAT::MAP;
+  if (argc == 1) {
+
+    std::printf("USAGE: %s [sample_size] [path_to_data] [data_format]\n",
+                argv[0]);
+
+    std::printf("Program running with default parameters\n sample_size = "
+                "%d\npath_to_data = %s\n data_format=%d\n ",
+                SAMPLE_SIZE, path_to_data, static_cast<int>(format));
+  } else if (argc >= 4) {
+    SAMPLE_SIZE = std::stoi(argv[1]);
+    pathString = argv[2];
+  }
+  Tokenizer token(path_to_data, SAMPLE_SIZE, Tokenizer::DATAFORMAT::MAP);
 
   std::vector<long long> freq = token.getFrequencies();
 
   std::vector<std::string> words = token.getWords();
-  Graph_Word_Freq(words, freq);
 
+  // histogram
+  /* Graph_Word_Freq(words, freq); */
+
+  // Zipf Distribution
+  Graph_Zipfs(freq);
   return 0;
 }
 
@@ -38,17 +60,19 @@ void Graph_Word_Freq(std::vector<std::string> &words,
 }
 void Graph_Zipfs(const std::vector<long long> &freq) {
   using namespace matplot;
-  // gca()->ylim(-); // Set y-axis limits from -5 to 5
 
   auto rank = linspace(0, freq.size(), freq.size());
   auto c = linspace(1, 10, rank.size());
+
+  std::vector<double> logRank(freq.size());
+  for (int i = 0; i < logRank.size(); i++) {
+    logRank[i] = std::log10(i);
+  }
 
   std::vector<double> fr(freq.size());
 
   for (int i = 0; i < fr.size(); i++)
     fr[i] = std::log10(freq[i]);
-  //  auto y = transform(freq, [&](long long x) { return static_cast<double>(x);
-  //  });
   auto current_axis = gca();
 
   // the font must be an Ethiopic type font in order to render the graph and
@@ -59,6 +83,9 @@ void Graph_Zipfs(const std::vector<long long> &freq) {
 
   current_axis->title("Zipf's Law on Amharic Language");
 
-  scatter(rank, fr, std::vector<double>{}, c);
+  scatter(logRank, fr, std::vector<double>{}, c);
+
+  //
+
   show();
 }
